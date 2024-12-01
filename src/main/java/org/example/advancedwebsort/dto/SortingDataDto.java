36bfc18350ef71b600697df07ac2e7bf080dc600
Mawule Gabriel Sakyi <gabriel.sakyi@amalitech.com>
2024-12-01 21:14:49 +0000

    feat: Implemented the SortingDataDto

---
+++ src/main/java/org/example/advancedwebsort/dto/SortingDataDto.java
@@ -0,0 +1,15 @@
+package org.example.advancedwebsort.dto;
+
+public class SortingDataDto {
+
+    private int[] data;
+
+    // Getter and Setter
+    public int[] getData() {
+        return data;
+    }
+
+    public void setData(int[] data) {
+        this.data = data;
+    }
+}
