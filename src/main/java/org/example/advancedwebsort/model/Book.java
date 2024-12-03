7630d44944716bd55f89af7571f0f8a56bdb5a70
Mawule Gabriel Sakyi <gabriel.sakyi@amalitech.com>
2024-12-03 14:15:37 +0000

    Added Javadocs comments to classses in the model package

--- src/main/java/org/example/advancedwebsort/model/Book.java
+++ src/main/java/org/example/advancedwebsort/model/Book.java
@@ -2,24 +2,47 @@ package org.example.advancedwebsort.model;
 
 import org.springframework.hateoas.RepresentationModel;
 
+/**
+ * Represents a book with an ID, title, and author.
+ * Extends {@link RepresentationModel} to support HATEOAS links.
+ */
 public class Book extends RepresentationModel<Book> {
     private Long id;
     private String title;
     private String author;
 
-    // Constructors, Getters, and Setters
+    /**
+     * Default constructor.
+     */
     public Book() {}
 
+    /**
+     * Constructor to initialize a book with an ID, title, and author.
+     *
+     * @param id the ID of the book.
+     * @param title the title of the book.
+     * @param author the author of the book.
+     */
     public Book(Long id, String title, String author) {
         this.id = id;
         this.title = title;
         this.author = author;
     }
 
+    /**
+     * Gets the ID of the book.
+     *
+     * @return the book's ID.
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Sets the ID of the book.
+     *
+     * @param id the book's ID.
+     */
     public void setId(Long id) {
         this.id = id;
     }
