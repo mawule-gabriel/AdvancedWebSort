134e9b49f8e9f997ed7c301f4d05d5be1edd1632
Mawule Gabriel Sakyi <gabriel.sakyi@amalitech.com>
2024-12-03 14:22:07 +0000

    Added JavaDocs comments to Websort class

--- src/main/java/org/example/advancedwebsort/Websort.java
+++ src/main/java/org/example/advancedwebsort/Websort.java
@@ -8,7 +8,10 @@ import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-
+/**
+ * Initializes the Spring web application by registering the {@link DispatcherServlet}.
+ * Configures the Spring application context and maps the servlet to the root URL ("/").
+ */
 public class Websort implements WebApplicationInitializer {
 
 
@@ -16,6 +19,12 @@ public class Websort implements WebApplicationInitializer {
         System.out.println("HelloWorld");
     }
 
+    /**
+     * Configures and initializes the {@link DispatcherServlet} for the web application.
+     *
+     * @param servletContext The servlet context to configure.
+     * @throws ServletException If an error occurs during servlet initialization.
+     */
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
